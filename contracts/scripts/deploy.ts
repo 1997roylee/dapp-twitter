77452254fb34f782e4731ca2add56355778872e3
Roy <53046899+1997roylee@users.noreply.github.com>
2023-01-03 00:49:50 +0800

    feat(contract): setup contracts

---
+++ contracts/scripts/deploy.ts
@@ -0,0 +1,18 @@
+import { ethers } from "hardhat";
+
+async function main() {
+  const contractFactory = await ethers.getContractFactory("HelloWorld");
+  const contract = await contractFactory.deploy();
+  await contract.deployed();
+  console.log("Contract deployed to:", contract.address);
+
+  const tweets = await contract.getTweets();
+  console.log("We got the tweets!", tweets);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
