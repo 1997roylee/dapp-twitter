6e798c7934f6ae81782303ebb96d77915fae796e
Roy <53046899+1997roylee@users.noreply.github.com>
2023-01-05 02:14:17 +0800

    feat(constants): create tweets list

---
+++ src/constants/tweets.ts
@@ -0,0 +1,21 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default [
+    {
+        id: '1',
+        author: "John Doe",
+        content: "12 months ago, I was Person of the Year",
+        date: "2020-01-01T00:00:00.000Z",
+    },
+    {
+        id: '2',
+        author: "John Doe",
+        content: "12 months ago, I was Person of the Year",
+        date: "2020-01-01T00:00:00.000Z",
+    },
+    {
+        id: '3',
+        author: "John Doe",
+        content: "12 months ago, I was Person of the Year",
+        date: "2020-01-01T00:00:00.000Z",
+    }
+]
