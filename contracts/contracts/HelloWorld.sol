057cf89d7c3845128210008ea03d4b50b8e1bb08
Roy <1997roylee@gmail.com>
2023-01-07 09:02:35 -0800

    feat(contract): add tip feature

--- contracts/contracts/HelloWorld.sol
+++ contracts/contracts/HelloWorld.sol
@@ -8,18 +8,53 @@ contract HelloWorld {
     struct Tweet {
         uint256 id;
         string hash;
+        uint256 tipAmount;
         address payable author;
     }
 
-    event CreateTweet(uint256 id, string hash, address payable author);
+    event CreateTweet(
+        uint256 id,
+        string hash,
+        uint256 tipAmount,
+        address payable author
+    );
+
+    event TipTweet(
+        uint256 id,
+        string hash,
+        uint256 tipAmount,
+        address payable author
+    );
 
     function mint(string memory _tokenURI) external returns (uint256) {
         tweetCount++;
-        tweets[tweetCount] = Tweet(tweetCount, _tokenURI, payable(msg.sender));
+        tweets[tweetCount] = Tweet(
+            tweetCount,
+            _tokenURI,
+            0,
+            payable(msg.sender)
+        );
         // emit event
-        emit CreateTweet(tweetCount, _tokenURI, payable(msg.sender));
+        emit CreateTweet(tweetCount, _tokenURI, 0, payable(msg.sender));
 
         return (tweetCount);
+    }
+
+    function tip(uint256 _id) external payable {
+        // make sure the id is valid
+        require(_id > 0 && _id <= tweetCount, "Invalid id");
+        // fetch the tweet
+        Tweet memory _tweet = tweets[_id];
+        // fetch the author
+        address payable _author = _tweet.author;
+        // pay the author by sending them Ether
+        _author.transfer(msg.value);
+        // increment the tip amount
+        _tweet.tipAmount = _tweet.tipAmount + msg.value;
+        // update the tweet
+        tweets[_id] = _tweet;
+        // trigger an event
+        emit TipTweet(_id, _tweet.hash, _tweet.tipAmount, _author);
     }
 
     function getTweets() public view returns (Tweet[] memory _tweets) {
--- contracts/scripts/deploy.ts
+++ contracts/scripts/deploy.ts
@@ -5,9 +5,15 @@ async function main() {
   const contract = await contractFactory.deploy();
   await contract.deployed();
   console.log("Contract deployed to:", contract.address);
+  // Mint a tweet
   const transaction = await contract.mint("http://google.com")
   await transaction.wait()
-
+  // Tip a tweet
+  const tipTransaction = await contract.tip(1, {
+    value: ethers.utils.parseEther("0.1")
+  })
+  await tipTransaction.wait()
+  // Get all tweets
   const tweets = await contract.getTweets();
   console.log("We got the tweets!", tweets);
 }
